9269eb14630330a1075332c9284806d3796b294e
JoEzif <kolofeez2004@hotmail.com>
2017-08-29 18:59:14 +0300

    sssssssssssssssss

--- Military_Services/Soldiers/Soldiers_Manage.Designer.cs
+++ Military_Services/Soldiers/Soldiers_Manage.Designer.cs
@@ -47,6 +47,7 @@
             this.soldiers_ViewTableAdapter = new Military_Services.Database_Connection.MilitaryViews_DatasetTableAdapters.Soldiers_ViewTableAdapter();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
@@ -58,7 +59,7 @@
             this.sarayaMainBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.saraya_MainTableAdapter = new Military_Services.Database_Connection.militaryDatasetTableAdapters.Saraya_MainTableAdapter();
             this.saraya_DetailsTableAdapter = new Military_Services.Database_Connection.militaryDatasetTableAdapters.Saraya_DetailsTableAdapter();
-            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.gvSoldiersData)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.soldiersViewBindingSource)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.militaryViews_Dataset)).BeginInit();
@@ -90,12 +91,13 @@
             this.gvAttachType_Details,
             this.gvAttachType_Date});
             this.gvSoldiersData.DataSource = this.soldiersViewBindingSource;
-            this.gvSoldiersData.Location = new System.Drawing.Point(6, 23);
+            this.gvSoldiersData.Location = new System.Drawing.Point(8, 28);
+            this.gvSoldiersData.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.gvSoldiersData.Name = "gvSoldiersData";
             this.gvSoldiersData.ReadOnly = true;
             this.gvSoldiersData.RowTemplate.Height = 24;
             this.gvSoldiersData.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.gvSoldiersData.Size = new System.Drawing.Size(1274, 303);
+            this.gvSoldiersData.Size = new System.Drawing.Size(1699, 373);
             this.gvSoldiersData.TabIndex = 0;
             this.gvSoldiersData.CellContentDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.gvSoldiersData_CellContentDoubleClick);
             // 
@@ -200,15 +202,18 @@
             // groupBox1
             // 
             this.groupBox1.Controls.Add(this.gvSoldiersData);
-            this.groupBox1.Location = new System.Drawing.Point(10, 137);
+            this.groupBox1.Location = new System.Drawing.Point(13, 169);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(1284, 350);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Size = new System.Drawing.Size(1712, 431);
             this.groupBox1.TabIndex = 1;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "بيانات الأفراد";
             // 
             // groupBox2
             // 
+            this.groupBox2.Controls.Add(this.button2);
             this.groupBox2.Controls.Add(this.button1);
             this.groupBox2.Controls.Add(this.label3);
             this.groupBox2.Controls.Add(this.label2);
@@ -216,48 +221,62 @@
             this.groupBox2.Controls.Add(this.txtSearch);
             this.groupBox2.Controls.Add(this.comboBox2);
             this.groupBox2.Controls.Add(this.comboBox1);
-            this.groupBox2.Location = new System.Drawing.Point(9, 10);
-            this.groupBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.groupBox2.Location = new System.Drawing.Point(12, 12);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.groupBox2.Size = new System.Drawing.Size(1101, 97);
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(4, 2, 4, 2);
+            this.groupBox2.Size = new System.Drawing.Size(1468, 119);
             this.groupBox2.TabIndex = 2;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "أدوات البحث";
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(32, 43);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(97, 44);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "اضافة اجازة ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(432, 54);
+            this.label3.Location = new System.Drawing.Point(576, 66);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(62, 13);
+            this.label3.Size = new System.Drawing.Size(65, 17);
             this.label3.TabIndex = 5;
             this.label3.Text = "حسب القسم:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(692, 54);
+            this.label2.Location = new System.Drawing.Point(923, 66);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(66, 13);
+            this.label2.Size = new System.Drawing.Size(71, 17);
             this.label2.TabIndex = 4;
             this.label2.Text = "حسب السرية:";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(996, 54);
+            this.label1.Location = new System.Drawing.Point(1328, 66);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.Size = new System.Drawing.Size(33, 17);
             this.label1.TabIndex = 3;
             this.label1.Text = "بحث:";
             // 
             // txtSearch
             // 
-            this.txtSearch.Location = new System.Drawing.Point(789, 51);
-            this.txtSearch.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtSearch.Location = new System.Drawing.Point(1052, 63);
+            this.txtSearch.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
             this.txtSearch.Name = "txtSearch";
-            this.txtSearch.Size = new System.Drawing.Size(202, 20);
+            this.txtSearch.Size = new System.Drawing.Size(268, 22);
             this.txtSearch.TabIndex = 2;
             this.txtSearch.TextChanged += new System.EventHandler(this.txtSearch_TextChanged);
             // 
@@ -266,10 +285,10 @@
             this.comboBox2.DataSource = this.sarayaDetailsBindingSource;
             this.comboBox2.DisplayMember = "SarayaDetails_Name";
             this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(278, 51);
-            this.comboBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.comboBox2.Location = new System.Drawing.Point(371, 63);
+            this.comboBox2.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
             this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(150, 21);
+            this.comboBox2.Size = new System.Drawing.Size(199, 24);
             this.comboBox2.TabIndex = 1;
             this.comboBox2.ValueMember = "SaryaDetails_Id";
             // 
@@ -288,10 +307,10 @@
             this.comboBox1.DataSource = this.sarayaMainBindingSource;
             this.comboBox1.DisplayMember = "MainSaraya_Name";
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(537, 51);
-            this.comboBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.comboBox1.Location = new System.Drawing.Point(716, 63);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(150, 21);
+            this.comboBox1.Size = new System.Drawing.Size(199, 24);
             this.comboBox1.TabIndex = 0;
             this.comboBox1.ValueMember = "MainSaraya_Id";
             // 
@@ -308,25 +327,25 @@
             // 
             this.saraya_DetailsTableAdapter.ClearBeforeFill = true;
             // 
-            // button1
+            // button2
             // 
-            this.button1.Location = new System.Drawing.Point(24, 35);
-            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(73, 36);
-            this.button1.TabIndex = 6;
-            this.button1.Text = "اضافة اجازة ";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button2.Location = new System.Drawing.Point(184, 43);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(97, 44);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "اضافة اجازة ";
+            this.button2.UseVisualStyleBackColor = true;
             // 
             // Soldiers_Manage
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(1306, 495);
+            this.ClientSize = new System.Drawing.Size(1741, 609);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Soldiers_Manage";
             this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
             this.RightToLeftLayout = true;
@@ -382,5 +401,6 @@
         private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
         private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
